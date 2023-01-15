39817cb12598256714780789ec6899f8fac02fcb
watanabe0055 <mailok1212+github@gmail.com>
2023-01-15 22:48:56 +0900

    fix: reset css install

--- frontend/snack-review-next/src/component/block/header/index.tsx
+++ frontend/snack-review-next/src/component/block/header/index.tsx
@@ -1,7 +1,165 @@
-import React from "react";
-import Header from "@/component/block/header/index";
+import {
+  createStyles,
+  Header,
+  HoverCard,
+  Group,
+  Button,
+  UnstyledButton,
+  Text,
+  SimpleGrid,
+  ThemeIcon,
+  Anchor,
+  Divider,
+  Center,
+  Box,
+  Burger,
+  Drawer,
+  Collapse,
+  ScrollArea,
+} from "@mantine/core";
 
-const Header = () => {
-  return <div>index</div>;
+import { useDisclosure } from "@mantine/hooks";
+
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+import logo from "@/img/logo/logo.svg";
+
+/* ヘッダーのデザインです
+TODO: mantineのドキュメントに記載してあった箇所をコピペしてるだけなので、不要な箇所あると思います*/
+const useStyles = createStyles((theme) => ({
+  link: {
+    display: "flex",
+    alignItems: "center",
+    height: "100%",
+    paddingLeft: theme.spacing.md,
+    paddingRight: theme.spacing.md,
+    textDecoration: "none",
+    color: theme.colorScheme === "dark" ? theme.white : theme.black,
+    fontWeight: 500,
+    fontSize: theme.fontSizes.sm,
+
+    [theme.fn.smallerThan("sm")]: {
+      height: 42,
+      display: "flex",
+      alignItems: "center",
+      width: "100%",
+    },
+
+    ...theme.fn.hover({
+      backgroundColor:
+        theme.colorScheme === "dark"
+          ? theme.colors.dark[6]
+          : theme.colors.gray[0],
+    }),
+  },
+
+  subLink: {
+    width: "100%",
+    padding: `${theme.spacing.xs}px ${theme.spacing.md}px`,
+    borderRadius: theme.radius.md,
+
+    ...theme.fn.hover({
+      backgroundColor:
+        theme.colorScheme === "dark"
+          ? theme.colors.dark[7]
+          : theme.colors.gray[0],
+    }),
+
+    "&:active": theme.activeStyles,
+  },
+
+  dropdownFooter: {
+    backgroundColor:
+      theme.colorScheme === "dark"
+        ? theme.colors.dark[7]
+        : theme.colors.gray[0],
+    margin: -theme.spacing.md,
+    marginTop: theme.spacing.sm,
+    padding: `${theme.spacing.md}px ${theme.spacing.md * 2}px`,
+    paddingBottom: theme.spacing.xl,
+    borderTop: `1px solid ${
+      theme.colorScheme === "dark" ? theme.colors.dark[5] : theme.colors.gray[1]
+    }`,
+  },
+
+  hiddenMobile: {
+    [theme.fn.smallerThan("sm")]: {
+      display: "none",
+    },
+  },
+
+  hiddenDesktop: {
+    [theme.fn.largerThan("sm")]: {
+      display: "none",
+    },
+  },
+}));
+
+//ヘッダーのコンポーネントです
+const HeaderBlock = () => {
+  const [drawerOpened, { toggle: toggleDrawer, close: closeDrawer }] =
+    useDisclosure(false);
+  const [linksOpened, { toggle: toggleLinks }] = useDisclosure(false);
+  const { classes, theme } = useStyles();
+
+  return (
+    <>
+      <Box pb={30}>
+        <Header height={80} px="md">
+          <Group position="apart" sx={{ height: "100%" }}>
+            <Image
+              src="logo.svg"
+              alt={"お菓子シェアサイトのロゴです"}
+              width={80}
+              height={69}
+            />
+            <Group className={classes.hiddenMobile}>
+              <Link href={"/login"}>
+                <Button variant="default">Log in</Button>
+              </Link>
+              <Link href={"/signup"}>
+                <Button>Sign up</Button>
+              </Link>
+            </Group>
+
+            <Burger
+              opened={drawerOpened}
+              onClick={toggleDrawer}
+              className={classes.hiddenDesktop}
+            />
+          </Group>
+        </Header>
+
+        {/* モーダルの領域 */}
+        <Drawer
+          opened={drawerOpened}
+          onClose={closeDrawer}
+          size="100%"
+          padding="md"
+          title="シェアがし"
+          className={classes.hiddenDesktop}
+          zIndex={1000000}
+        >
+          <ScrollArea sx={{ height: "calc(100vh - 60px)" }} mx="-md">
+            <Divider
+              my="sm"
+              color={theme.colorScheme === "dark" ? "dark.5" : "gray.1"}
+            />
+
+            <Divider
+              my="sm"
+              color={theme.colorScheme === "dark" ? "dark.5" : "gray.1"}
+            />
+
+            <Group position="center" grow pb="xl" px="md">
+              <Button variant="default">Log in</Button>
+              <Button>Sign up</Button>
+            </Group>
+          </ScrollArea>
+        </Drawer>
+      </Box>
+    </>
+  );
 };
-export default Header;
+export default HeaderBlock;
--- frontend/snack-review-next/src/pages/index.tsx
+++ frontend/snack-review-next/src/pages/index.tsx
@@ -1,6 +1,8 @@
+import HeaderBlock from "@/component/block/header/index";
 export default function Home() {
   return (
     <>
+      <HeaderBlock />
       <p>TOP</p>
     </>
   );
