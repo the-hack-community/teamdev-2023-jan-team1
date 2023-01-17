0003a8655c749ab0194c080604b62486e2521431
watanabe0055 <mailok1212+github@gmail.com>
2023-01-18 01:18:47 +0900

    fix: rest

--- frontend/snack-review-next/src/pages/_app.tsx
+++ frontend/snack-review-next/src/pages/_app.tsx
@@ -1,5 +1,6 @@
 import "../../styles/globals.css";
 import "modern-css-reset/dist/reset.min.css";
+
 import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
