28c272cc47f2c285f450a57cbcb3d38dad8cdd67
mu0363 <dde036@gmail.com>
2023-01-30 16:31:33 +0900

    styles: 全体のフォントと文字色を変更
    フォント: M_PLUS_Rounded_1c
    文字色: text-gray-700

--- frontend/snack-review-next/src/app/layout.tsx
+++ frontend/snack-review-next/src/app/layout.tsx
@@ -1,9 +1,17 @@
+// eslint-disable-next-line camelcase
+import { M_PLUS_Rounded_1c } from "@next/font/google";
 import type { ReactNode } from "react";
 import "../styles/globals.css";
 
+const mPlus = M_PLUS_Rounded_1c({
+  subsets: ["japanese"],
+  weight: ["400", "700"],
+  display: "swap",
+});
+
 const RootLayout = ({ children }: { children: ReactNode }) => {
   return (
-    <html lang="ja">
+    <html lang="ja" data-theme="light" className={`${mPlus.className} text-gray-700`}>
       <head />
       <body>{children}</body>
     </html>
