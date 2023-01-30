7331ebd955512eb570059efd87fa6906d27c8de6
watanabe0055 <mailok1212+github@gmail.com>
2023-01-30 23:54:35 +0900

    fix: typeのふよ

--- frontend/snack-review-next/src/component/atoms/form/InputField.tsx
+++ frontend/snack-review-next/src/component/atoms/form/InputField.tsx
@@ -10,6 +10,7 @@ export const InputField: FC<Props> = ({ label, isRequired, ...restProps }) => {
         {isRequired && <span className="text-lg text-red-400">*</span>}
         <div>
           <input
+            type={restProps.id}
             className="w-full appearance-none rounded-lg border border-gray-400 py-2 px-3 leading-tight text-gray-700 placeholder:font-light placeholder:text-gray-300 focus:border-blue-400 focus:outline-none"
             {...restProps}
           />
