c404173483d303750d9996bcfd9a629159afd327
watanabe0055 <mailok1212+github@gmail.com>
2023-01-30 23:50:00 +0900

    feat: マークアップ

--- frontend/snack-review-next/src/constants/InputField.ts
+++ frontend/snack-review-next/src/constants/InputField.ts
@@ -1,6 +1,14 @@
 type InputKeyType = "id" | "label" | "placeholder" | "isRequired";
 
 // メールフォームの定数
+export const USER_NAME_FIELD = {
+  label: "ユーザー名",
+  id: "text",
+  placeholder: "username",
+  isRequired: true,
+} satisfies Record<InputKeyType, string | boolean>;
+
+// メールフォームの定数
 export const EMAIL_FIELD = {
   label: "メールアドレス",
   id: "email",
@@ -11,6 +19,14 @@ export const EMAIL_FIELD = {
 // パスワードフォームの定数
 export const PASSWORD_FIELD = {
   label: "パスワード",
+  id: "password",
+  placeholder: "your password",
+  isRequired: true,
+} satisfies Record<InputKeyType, string | boolean>;
+
+// パスワード確認フォームの定数
+export const PASSWORD_FIELD_VERIFICATION = {
+  label: "パスワード確認",
   id: "password",
   placeholder: "your password",
   isRequired: true,
