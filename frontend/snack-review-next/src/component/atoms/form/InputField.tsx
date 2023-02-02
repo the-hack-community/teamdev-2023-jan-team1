bbb083d4a0feb5625048024f1e0dede911e3229b
watanabe0055 <mailok1212+github@gmail.com>
2023-02-03 07:54:03 +0900

    fix: 修正

--- frontend/snack-review-next/src/component/atoms/form/InputField.tsx
+++ frontend/snack-review-next/src/component/atoms/form/InputField.tsx
@@ -1,8 +1,8 @@
 import type { FC, ComponentProps } from "react";
 
-type Props = ComponentProps<"input"> & { label: string; isRequired: boolean; type: string };
+type Props = ComponentProps<"input"> & { label: string; isRequired: boolean };
 
-export const InputField: FC<Props> = ({ label, isRequired, type, ...restProps }) => {
+export const InputField: FC<Props> = ({ label, isRequired, ...restProps }) => {
   return (
     <div className="w-full">
       <label htmlFor={label} className="block text-sm text-gray-700">
@@ -10,7 +10,6 @@ export const InputField: FC<Props> = ({ label, isRequired, type, ...restProps })
         {isRequired && <span className="text-lg text-red-400">*</span>}
         <div>
           <input
-            type={type}
             className="w-full appearance-none rounded-lg border border-gray-400 py-2 px-3 leading-tight text-gray-700 placeholder:font-light placeholder:text-gray-300 focus:border-blue-400 focus:outline-none"
             {...restProps}
           />
