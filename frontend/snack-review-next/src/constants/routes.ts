d6b33a3550fb6d6ae0c7c651a1dcefcc0901690a
mu0363 <dde036@gmail.com>
2023-02-03 10:42:56 +0900

    feat: サーバーコンポーネントでのデータ取得関数を作成

---
+++ frontend/snack-review-next/src/constants/routes.ts
@@ -0,0 +1,4 @@
+export const ROOT_PATH = "/";
+export const MY_PROFILE_PATH = "/my-profile";
+export const POST_ARTICLE_PATH = "/post-article";
+export const articlePath = (articleId: string) => `/article/${articleId}`;
