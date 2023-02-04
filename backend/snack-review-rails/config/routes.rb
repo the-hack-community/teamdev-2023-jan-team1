a09175b6ebdedfe02e725bc66bd7b7b152413cdf
takeshi_honda <honda0924@hotmail.co.jp>
2023-02-04 23:42:21 +0900

    feat: routing

--- backend/snack-review-rails/config/routes.rb
+++ backend/snack-review-rails/config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
   namespace 'api' do
     namespace 'v1' do 
       mount_devise_token_auth_for "User", at: "auth"
+      resources :articles, except: [:new, :edit]
     end
   end
 end
