14e8bf9c1203179bc39516a8a2c0be60f2365135
mu0363 <dde036@gmail.com>
2023-02-07 18:47:54 +0900

    feat: 記事投稿画面を作成

---
+++ frontend/snack-review-next/src/app/post-article/page.tsx
@@ -0,0 +1,31 @@
+"use client";
+
+import type { ComponentProps } from "react";
+import { CommonButton } from "@/component/atoms/button/CommonButton";
+import { InputField } from "@/component/atoms/form/InputField";
+import { SelectField } from "@/component/atoms/form/SelectField";
+import { TextAreaField } from "@/component/atoms/form/TextAreaField";
+
+const PostArticle = () => {
+  const handleClick: ComponentProps<"button">["onClick"] = (e) => {
+    e.preventDefault();
+  };
+
+  return (
+    <div className="flex-1 px-6">
+      <h1 className="my-12 text-center text-lg font-bold text-blue-400">自分の感想を共有しよう!</h1>
+      <div className="mb-10 grid grid-cols-1 gap-7">
+        <InputField isRequired label="タイトル" placeholder="タイトルを入れましょう" />
+        <TextAreaField isRequired label="コンテンツ" placeholder="自分の感想を共有しましょう。" />
+        <SelectField isRequired label="種類" placeholder="お菓子の種類を選択" />
+        <InputField isRequired label="お店のURL" placeholder="https://example.com" />
+        <TextAreaField isRequired={false} label="お店の情報" placeholder="東京都港区青山1-1-1" />
+        <CommonButton isFullWidth isPrimary={false} handleClick={handleClick}>
+          投稿内容を確認する
+        </CommonButton>
+      </div>
+    </div>
+  );
+};
+
+export default PostArticle;
