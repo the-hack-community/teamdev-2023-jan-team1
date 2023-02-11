9594ba2f9d1f915633740db872d3395dae02e3dd
takeshi_honda <honda0924@hotmail.co.jp>
2023-02-11 17:16:14 +0900

    feat: index, create, update, destroy test

--- backend/snack-review-rails/spec/support/authorization_spec.rb
+++ backend/snack-review-rails/spec/support/authorization_spec.rb
@@ -1,7 +1,9 @@
 module AuthorizationHelper
   def sign_in(user)
     post 'http://localhost:3001/api/v1/auth/sign_in', params: { email: user.email, password: user.password }
-    # レスポンスのHeadersからトークン認証に必要な要素を抜き出して返す処理
     response.headers.slice('client', 'uid', 'token-type', 'access-token')
+  end
+  def sign_out(user)
+    delete 'http://localhost:3001/api/v1/auth/sign_out'
   end
 end
