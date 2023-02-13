df3a44bab3bc36b6b1ef11f52ad77697878c2946
watanabe0055 <mailok1212+github@gmail.com>
2023-02-14 01:03:04 +0900

    fix: validation

--- frontend/snack-review-next/src/component/atoms/form/InputField.tsx
+++ frontend/snack-review-next/src/component/atoms/form/InputField.tsx
@@ -8,6 +8,19 @@ type Props = ComponentProps<"input"> & { label?: string; isRequired: boolean; va
 export const InputField: FC<Props> = ({ label, isRequired, validation, ...restProps }) => {
   const inputRef = useAutoFocus<HTMLInputElement>();
 
+  // バリデーション時の赤文字
+  const validationCheck = () => {
+    if (validation) {
+      const borderColor = " border-red-400";
+      const placeholderColor = "placeholder:text-red-300";
+      return { borderColor, placeholderColor };
+    }
+    const borderColor = " border-gray-400";
+    const placeholderColor = "placeholder:text-gray-300";
+    return { borderColor, placeholderColor };
+  };
+  const color = validationCheck();
+
   return (
     <div className="w-full">
       {label && (
@@ -19,7 +32,7 @@ export const InputField: FC<Props> = ({ label, isRequired, validation, ...restPr
       <div>
         <input
           ref={inputRef}
-          className="h-9 w-full appearance-none rounded-lg border border-gray-400 px-3 leading-tight text-gray-700 placeholder:font-light placeholder:text-gray-300 focus:border-blue-400 focus:outline-none"
+          className={`h-9 w-full appearance-none rounded-lg border ${color.borderColor} px-3 leading-tight text-gray-700 placeholder:font-light ${color.placeholderColor}  focus:border-blue-400 focus:outline-none`}
           {...restProps}
         />
         <p className="text-red-400">{validation}</p>
