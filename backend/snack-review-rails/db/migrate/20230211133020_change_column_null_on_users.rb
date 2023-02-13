cd8939fd861afc9e49653d506d118781e4ed704f
takeshi_honda <honda0924@hotmail.co.jp>
2023-02-13 22:34:30 +0900

    fix: migration

--- backend/snack-review-rails/db/migrate/20230211133020_change_column_null_on_users.rb
+++ backend/snack-review-rails/db/migrate/20230211133020_change_column_null_on_users.rb
@@ -1,5 +1,5 @@
 class ChangeColumnNullOnUsers < ActiveRecord::Migration[7.0]
   def change
-    change_column_null :users, :name, false
+    change_column_null :users, :name, null: false
   end
 end
