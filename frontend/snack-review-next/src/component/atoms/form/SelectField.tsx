7caf3f5afd2493201da89f72525dc491975648c5
mu0363 <dde036@gmail.com>
2023-02-14 13:58:41 +0900

    refactor: SelectFieldコンポーネントのvalueを変更

--- frontend/snack-review-next/src/component/atoms/form/SelectField.tsx
+++ frontend/snack-review-next/src/component/atoms/form/SelectField.tsx
@@ -3,7 +3,7 @@
 import { FormLabel } from "./FormLabel";
 import type { ComponentProps, FC } from "react";
 
-type Props = ComponentProps<"input"> & { label?: string; isRequired: boolean };
+type Props = ComponentProps<"select"> & { label?: string; isRequired: boolean };
 
 export const SelectField: FC<Props> = ({ label, isRequired, ...restProps }) => {
   return (
@@ -13,15 +13,18 @@ export const SelectField: FC<Props> = ({ label, isRequired, ...restProps }) => {
           {label}
         </FormLabel>
       )}
-      <select className="select w-full rounded-lg border border-gray-400 bg-white px-3 text-base font-normal leading-tight placeholder:font-light placeholder:text-gray-300 focus:border-blue-400 focus:outline-none">
-        <option disabled selected>
+      <select
+        {...restProps}
+        className="select w-full rounded-lg border border-gray-400 bg-white px-3 text-base font-normal leading-tight placeholder:font-light placeholder:text-gray-300 focus:border-blue-400 focus:outline-none"
+      >
+        <option disabled value="default">
           お菓子の種類を選択
         </option>
-        <option value="baked">焼き菓子</option>
-        <option value="cake">ケーキ</option>
-        <option value="chocolate">チョコ</option>
-        <option value="japanese-sweets">和菓子</option>
-        <option value="others">その他</option>
+        <option value="焼き菓子">焼き菓子</option>
+        <option value="ケーキ">ケーキ</option>
+        <option value="チョコ">チョコ</option>
+        <option value="和菓子">和菓子</option>
+        <option value="その他">その他</option>
       </select>
     </div>
   );
