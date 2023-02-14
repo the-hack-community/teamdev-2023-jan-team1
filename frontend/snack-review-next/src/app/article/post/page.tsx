a71bd68754aab2e9e343ad2c16050bfa3b512d06
mu0363 <dde036@gmail.com>
2023-02-14 11:49:32 +0900

    refactor: 記事投稿ページをarticleディレクトリへ移動

--- frontend/snack-review-next/src/app/article/post/page.tsx
+++ frontend/snack-review-next/src/app/article/post/page.tsx
@@ -7,6 +7,7 @@ import { ConfirmModal } from "@/component/atoms/card/ConfirmModal";
 import { InputField } from "@/component/atoms/form/InputField";
 import { SelectField } from "@/component/atoms/form/SelectField";
 import { TextAreaField } from "@/component/atoms/form/TextAreaField";
+import { ARTICLE_CONTENT, ARTICLE_SHOP_INFO, ARTICLE_SHOP_URL, ARTICLE_TITLE, CATEGORY } from "@/constants/InputField";
 
 const PostArticle = () => {
   const [isOpen, setIsOpen] = useState(false);
@@ -21,11 +22,11 @@ const PostArticle = () => {
     <div className="flex-1 px-6">
       <h1 className="my-12 text-center text-lg font-bold text-blue-400">自分の感想を共有しよう!</h1>
       <div className="mb-10 grid grid-cols-1 gap-7">
-        <InputField isRequired label="タイトル" placeholder="タイトルを入れましょう" />
-        <TextAreaField isRequired label="コンテンツ" placeholder="自分の感想を共有しましょう。" />
-        <SelectField isRequired label="種類" placeholder="お菓子の種類を選択" />
-        <InputField isRequired label="お店のURL" placeholder="https://example.com" />
-        <TextAreaField isRequired={false} label="お店の情報" placeholder="東京都港区青山1-1-1" />
+        <InputField {...ARTICLE_TITLE} />
+        <TextAreaField {...ARTICLE_CONTENT} />
+        <SelectField {...CATEGORY} />
+        <InputField {...ARTICLE_SHOP_URL} />
+        <TextAreaField {...ARTICLE_SHOP_INFO} />
         <CommonButton isFullWidth isPrimary={false} handleClick={handleClick}>
           投稿内容を確認する
         </CommonButton>
