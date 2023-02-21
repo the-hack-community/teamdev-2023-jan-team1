cf2c96f9c9f695b8d434f59c4e4943465ec12fd0
maeken6319 <nr8317649@gmail.com>
2023-02-21 20:12:55 +0900

    my_profiles、categoriesのRSpecを追記

--- backend/snack-review-rails/spec/requests/api/v1/categories_spec.rb
+++ backend/snack-review-rails/spec/requests/api/v1/categories_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe "Api::V1::Categories", type: :request do
   describe "GET /index" do
     it "returns http success" do
-      get "/api/v1/categories/index"
+      get "http://localhost:3001/api/v1/categories"
       expect(response).to have_http_status(:success)
     end
   end
