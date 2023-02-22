b35b65311262e29cdd312264094371e8c4734f3f
mu0363 <dde036@gmail.com>
2023-02-22 13:15:20 +0900

    fix: ログイン・ログアウトのエラーを解消

--- frontend/snack-review-next/src/component/template/footer/Footer.tsx
+++ frontend/snack-review-next/src/component/template/footer/Footer.tsx
@@ -1,21 +1,16 @@
 "use client";
 
-import Cookies from "js-cookie";
 import { FooterLoggedIn } from "./FooterLoggedIn";
 import { FooterNotLoggedIn } from "./FooterNotLoggedIn";
-import { Loader } from "@/component/atoms/Loader";
 import { useUser } from "@/lib/useUser";
 
-export const Footer = () => {
+export const Footer = ({ token }: { token: string | undefined }) => {
   const { data } = useUser();
-  const token = Cookies.get("access-token");
-
-  if (!data) return <Loader />;
 
   return (
     <footer className="fixed bottom-0 mt-6 w-full bg-gray-100 py-4 align-middle text-gray-700">
       <nav className={`${data ? "mx-4" : "mx-3"} flex items-center justify-between`}>
-        {token ? <FooterLoggedIn userName={data.user.name} /> : <FooterNotLoggedIn />}
+        {data && token ? <FooterLoggedIn userName={data.user?.name} /> : <FooterNotLoggedIn />}
       </nav>
     </footer>
   );
